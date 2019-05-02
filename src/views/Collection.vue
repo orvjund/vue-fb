25f2d318bf7bcfe8e111b8bfb684547fdbbfea1a
Huy Hoàng Phan <hoangph271@gmail.com>
2019-05-02 11:28:12 +0700

    Use howler.js as media player

--- src/views/Collection.vue
+++ src/views/Collection.vue
@@ -14,7 +14,8 @@
       <div
         v-for="song in songs"
         :key="song.id"
-        class="song"
+        :class="{'song': true, 'selected-song': currentSongId === song.id}"
+        @click="() => currentSongId = song.id"
       >
         {{ song.title }}
       </div>
@@ -24,6 +25,7 @@
 
 <script>
 import { firestore } from '@/firetools';
+import { Howl } from 'howler';
 
 export default {
   name: 'Collection',
@@ -31,6 +33,8 @@ export default {
     selectedCollection: null,
     collections: [],
     songs: [],
+    currentSongId: '',
+    thePlayer: null,
   }),
   watch: {
     async selectedCollection() {
@@ -43,10 +47,51 @@ export default {
         this.songs = this.selectedCollection.songs;
       }
     },
+    currentSongId() {
+      if (this.currentSongId === '') return;
+
+      const { url } = this.songs.find(song => song.id === this.currentSongId);
+
+      if (this.thePlayer) this.thePlayer.unload();
+
+      this.thePlayer = new Howl({
+        src: [url],
+        html5: true,
+      });
+      this.thePlayer.on('end', () => {
+        this.nextSong();
+      });
+      this.thePlayer.play();
+    },
   },
   async mounted() {
     this.collections = await firestore.listCollection('collections');
     [this.selectedCollection] = this.collections;
   },
+  methods: {
+    nextSong() {
+      const currentSongIndex = this.songs.findIndex(song => song.id === this.currentSongId);
+
+      if (currentSongIndex + 1 >= this.songs.length) return;
+
+      this.currentSongId = this.songs[currentSongIndex + 1].id;
+    },
+  },
 };
 </script>
+
+<style scoped>
+.song {
+  margin: 5px auto;
+  border: 1px solid gray;
+  cursor: pointer;
+  width: 20em;
+  border-radius: 0.25em;
+}
+.song:hover, .selected-song {
+  background-color: lightgray;
+}
+.selected-song {
+  cursor: default;
+}
+</style>
